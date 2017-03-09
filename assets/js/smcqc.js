0e78db64ee7d57736a08a7a475fa22dc6e7df0c9
AstroBolt <nicoesber21@gmail.com>
2017-03-09 17:23:14 +0800

    home-page

---
+++ assets/js/smcqc.js
@@ -0,0 +1,28 @@
+window.onscroll = function() {
+  var el = document.getElementsByClassName('header')[0];
+  var className = 'small';
+  if (el.classList) {
+    if (window.scrollY > 10)
+      el.classList.add(className);
+    else
+      el.classList.remove(className);
+  }
+};
+
+
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+function scrollTo() {
+  $('html, body').animate({ scrollTop: $('#footer').offset().top }, 'slow');
+  return false;
+}
